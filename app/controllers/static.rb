7bc0d4f65b872f6db1a73d89fa2e9380407cf41f
nextacademy <nextacademy@email.com>
2015-10-13 11:53:02 +0800

    added user profile page

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -7,12 +7,6 @@ get '/sign_up' do
 	erb :"sign_up"
 end
 
-#Creates User
-post '/sign_up/create' do
-	@creator = User.create(params[:user])
-	redirect '/'
-end
-
 #Property Page
 get '/property_view' do
 	erb :"property_view"
@@ -23,8 +17,29 @@ get '/property_create' do
 	erb :"property_create"
 end
 
+#User Profile Page
+get '/user_profile' do
+	erb :"user_profile"
+end
+
+#Show all Users
+post '/show_user' do
+	@view = users.all
+end
+
+#Creates User
+post '/sign_up/create' do
+	@creator = User.create(params[:user])
+	redirect '/'
+end
+
 #Creates Property
 post '/property_new/create' do
+	@finder = User.find_by(username: params[:username])
+	@finder
+
 	@property = Property.create(params[:property])
+	@property.user = @finder
+
 	redirect '/property_view'
 end
