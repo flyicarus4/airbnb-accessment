5fc23083daa8eca844c02f476f80c8324e0bf742
nextacademy <nextacademy@email.com>
2015-10-13 10:02:04 +0800

    check db and created sign_up page

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,8 @@
 get '/' do
   erb :"static/index"
 end
+
+#Create User
+get '/sign_up' do
+	erb :""
+end
--- db/migrate/20151013093205_create_properties.rb
+++ db/migrate/20151013093205_create_properties.rb
@@ -1,4 +1,11 @@
 class CreateProperties < ActiveRecord::Migration
 	def change
+		create_table :properties do |t|
+			t.string :location
+			t.string :availbility
+			t.belongs_to :user
+
+			t.timestamps null: false
+		end
 	end
 end
